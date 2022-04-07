bebf1375eceeaad3694d8c17c2408a6912adf2b5
Luis Fernando <luisfernandobr2003@gmail.com>
2022-04-07 17:20:40 -0300

    CadastroService update

---
+++ UsuariosApi/Services/EmailService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UsuariosApi.Services
+{
+    public class EmailService
+    {
+        public void EnviarEmail(string[] destinatario, string assunto, int id, string code)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
