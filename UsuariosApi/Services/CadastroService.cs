036fc2509d4d10270bb4ad20fc6468176170428b
Luis Fernando <luisfernandobr2003@gmail.com>
2022-04-07 16:02:24 -0300

    udpate Cadastro Controller and Service

--- UsuariosApi/Services/CadastroService.cs
+++ UsuariosApi/Services/CadastroService.cs
@@ -25,9 +25,14 @@ namespace UsuariosApi.Services
         {
             Usuario usuario = _mapper.Map<Usuario>(createDto);
             IdentityUser<int> usuarioIdentity = _mapper.Map<IdentityUser<int>>(usuario);
-            Task<IdentityResult> resultadoIdentity = _userManager.CreateAsync(usuarioIdentity, createDto.Password);
-            if (resultadoIdentity.Result.Succeeded) return Result.Ok();
-            return Result.Fail("Falha ao cadastrar usuário");
+            Task<IdentityResult> resultadoIdentity = _userManager
+                .CreateAsync(usuarioIdentity, createDto.Password);
+            if (resultadoIdentity.Result.Succeeded)
+            {
+                var code = _userManager.GenerateEmailConfirmationTokenAsync(usuarioIdentity).Result;
+                return Result.Ok().WithSuccess(code);
+            }
+                return Result.Fail("Falha ao cadastrar usuário");
 
         }
     }
--- UsuariosApi/Startup.cs
+++ UsuariosApi/Startup.cs
@@ -1,19 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using UsuariosApi.Data;
 using UsuariosApi.Services;
 
@@ -33,7 +26,9 @@ namespace UsuariosApi
         {
             services.AddDbContext<UserDbContext>(options => options.UseMySQL(Configuration.GetConnectionString("UsuarioConnection")));
             services
-                .AddIdentity<IdentityUser<int>, IdentityRole<int>>()
+                .AddIdentity<IdentityUser<int>, IdentityRole<int>>(
+                    opt => opt.SignIn.RequireConfirmedEmail = true
+                )
                 .AddEntityFrameworkStores<UserDbContext>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
