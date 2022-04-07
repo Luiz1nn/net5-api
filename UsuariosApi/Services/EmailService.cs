5de20de41541537464fb21d5604a817d6804f387
Luis Fernando <luisfernandobr2003@gmail.com>
2022-04-07 17:38:20 -0300

    Update EmailService

--- UsuariosApi/Services/EmailService.cs
+++ UsuariosApi/Services/EmailService.cs
@@ -1,12 +1,53 @@
-﻿using System;
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using System;
+using UsuariosApi.Models;
 
 namespace UsuariosApi.Services
 {
     public class EmailService
     {
-        public void EnviarEmail(string[] destinatario, string assunto, int id, string code)
+        public void EnviarEmail(string[] destinatario, string assunto, int usuarioId, string code)
         {
-            throw new NotImplementedException();
+            Mensagem mensagem = new Mensagem(destinatario, assunto, usuarioId, code);
+            var mensagemDeEmail = CriaCorpoDoEmail(mensagem);
+            Enviar(mensagemDeEmail);
+        }
+
+        private void Enviar(MimeMessage mensagemDeEmail)
+        {
+            using(var client = new SmtpClient())
+            {
+                try
+                {
+                    client.Connect("Conexão a fazer");
+                    // TODO Auth de e-mail
+                    client.Send(mensagemDeEmail);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                finally
+                {
+                    client.Disconnect(true);
+                    client.Dispose();
+                }
+            }
+        }
+
+        private MimeMessage CriaCorpoDoEmail(Mensagem mensagem)
+        {
+            var mensagemDeEmail = new MimeMessage();
+            mensagemDeEmail.From.Add(new MailboxAddress("ADICIONAR O REMETENTE"));
+            mensagemDeEmail.To.AddRange(mensagem.Destinatario);
+            mensagemDeEmail.Subject = mensagem.Assunto;
+            mensagemDeEmail.Body = new TextPart(MimeKit.Text.TextFormat.Text)
+            {
+                Text = mensagem.Conteudo
+            };
+
+            return mensagemDeEmail;
         }
     }
 }
