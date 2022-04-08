d0f7ef530523c4f97324d275c99fd62a30bf4874
Luis Fernando <luisfernandobr2003@gmail.com>
2022-04-08 11:12:40 -0300

    update LoginService

--- UsuariosApi/Services/LoginService.cs
+++ UsuariosApi/Services/LoginService.cs
@@ -7,36 +7,48 @@ using UsuariosApi.Models;
 using UsuariosApi.Services;
 
 public class LoginService
-    {
-        private SignInManager<IdentityUser<int>> _signInManager;
-        private TokenService _tokenService;
+{
+    private SignInManager<IdentityUser<int>> _signInManager;
+    private TokenService _tokenService;
 
-        public LoginService(SignInManager<IdentityUser<int>> signInManager,
-            TokenService tokenService)
+    public LoginService(SignInManager<IdentityUser<int>> signInManager, TokenService tokenService)
+    {
+        _signInManager = signInManager;
+        _tokenService = tokenService;
+    }
+
+    public Result LogaUsuario(LoginRequest request)
+    {
+        var resultadoIdentity = _signInManager
+            .PasswordSignInAsync(request.Username, request.Password, false, false);
+        if (resultadoIdentity.Result.Succeeded)
         {
-            _signInManager = signInManager;
-            _tokenService = tokenService;
+            var identityUser = _signInManager
+                .UserManager
+                .Users
+                .FirstOrDefault(usuario =>
+                usuario.NormalizedUserName == request.Username.ToUpper());
+            Token token = _tokenService.CreateToken(identityUser);
+            return Result.Ok().WithSuccess(token.Value);
+        }
+        return Result.Fail("Login falhou");
+    }
+
+    public Result SolicitaResetSenhaUsuario(SolicitaResetRequest request)
+    {
+        IdentityUser<int> identityUser = _signInManager
+            .UserManager
+            .Users
+            .FirstOrDefault(u => u.NormalizedEmail == request.Email.ToUpper());
+
+        if (identityUser != null)
+        {
+            string codigoDeRecuperacao = _signInManager
+                .UserManager.GeneratePasswordResetTokenAsync(identityUser).Result;
+
+            return Result.Ok().WithSuccess(codigoDeRecuperacao);
         }
 
-        public Result LogaUsuario(LoginRequest request)
-        {
-            var resultadoIdentity = _signInManager
-                .PasswordSignInAsync(request.Username, request.Password, false, false);
-            if (resultadoIdentity.Result.Succeeded)
-            {
-                var identityUser = _signInManager
-                    .UserManager
-                    .Users
-                    .FirstOrDefault(usuario => 
-                    usuario.NormalizedUserName == request.Username.ToUpper());
-                Token token = _tokenService.CreateToken(identityUser);
-                return Result.Ok().WithSuccess(token.Value);
-            }
-            return Result.Fail("Login falhou");
-        }
-
-    internal Result SolicitaResetSenhaUsuario(SolicitaResetRequest request)
-    {
-        throw new NotImplementedException();
+        return Result.Fail("Falha ao solicitar redefinição");
     }
 }
